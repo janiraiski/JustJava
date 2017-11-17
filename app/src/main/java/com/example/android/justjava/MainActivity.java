45087bd5ec150206ddfb5b8d6c5cd34a0da42547
Jani Raiski <jani.raiski@gmail.com>
2017-11-17 02:35:34 +0200

    Added string to submitOrder()

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -44,7 +44,8 @@ public class MainActivity extends AppCompatActivity
      */
     public void submitOrder(View view)
     {
-        displayPrice(quantity * 5);
+        String priceMessage = "Free";
+        displayMessage(priceMessage);
     }
 
     /**
@@ -63,5 +64,13 @@ public class MainActivity extends AppCompatActivity
     {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
